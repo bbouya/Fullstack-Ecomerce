d77a3f2722b295f34613598f92055cd492482c87
EL GHAFRAOUI AYOUB <52068209+bbouya@users.noreply.github.com>
2023-02-01 20:22:47 +0100

    files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 //import logo from './logo.svg';
 //import './App.css';
+import Footer from './componenets/Footer.js';
 import Header from  './componenets/Header.js';
+
+import {Container} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <main>
+          <Container>
+            <h1>Welcom to</h1>
+          </Container>
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
