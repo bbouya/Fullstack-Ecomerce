a7381cb6fb460f5527c8522ea4ceec3365a62431
EL GHAFRAOUI AYOUB <52068209+bbouya@users.noreply.github.com>
2023-02-02 12:36:01 +0100

    files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,24 +3,25 @@
 import Footer from './componenets/Footer.js';
 import Header from  './componenets/Header.js';
 
+import { BrowserRouter as router, Route } from 'react-router-dom';
+
 import {Container} from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen.js';
 
 function App() {
   return (
-    <div className="App">
+    <router>
       <Header></Header>
       <main className='py-3'>
           <Container>
-           <HomeScreen></HomeScreen>
+           <Route path = '/' component = {HomeScreen}/>
           </Container>
           <Container>
             <h3>hadi pour le test</h3>
           </Container>
       </main>
-      
       <Footer></Footer>
-    </div>
+    </router>
   );
 }
 
